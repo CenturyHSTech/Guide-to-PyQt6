bc90eeee4cd9e02880219856e3fdc58319e4a634
Hundredvisionsguy <winikkc@hsd.k12.or.us>
2024-05-03 16:01:49 -0700

    adding stylized title screen

--- guide_to_pyqt6/title_screen.py
+++ guide_to_pyqt6/title_screen.py
@@ -1,11 +1,11 @@
 from PyQt6.QtCore import QEvent, QSize, Qt
-from PyQt6.QtGui import QIcon
+from PyQt6.QtGui import QIcon, QFont, QFontDatabase, QColor
 from PyQt6.QtWidgets import (
     QApplication,
+    QGraphicsDropShadowEffect,
     QHBoxLayout,
     QLabel,
     QMainWindow,
-    QStyle,
     QToolButton,
     QVBoxLayout,
     QWidget,
@@ -17,24 +17,30 @@ python_gray = "#646464"
 python_yellow = "#FFD43B"
 python_light_yellow = "#FFE873"
 
+
 class CustomTitleBar(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.initial_pos = None
         title_bar_layout = QHBoxLayout(self)
-        title_bar_layout.setContentsMargins(1, 1, 1, 1)
-        title_bar_layout.setSpacing(2)
+        title_bar_layout.setContentsMargins(1, 1, 10, 1)
+        title_bar_layout.setSpacing(3)
         self.title = QLabel(f"{self.__class__.__name__}", self)
         self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.title.setStyleSheet(
             """
-        QLabel { text-transform: uppercase; font-size: 10pt; margin-left: 48px; }
-        """
-        )
+            QLabel { font-family: "Titillium Web", sans-serif;
+                font-size: 16pt; margin-left: 48px;
+                color: #002b44; padding: 4px;
+                font-weight: 600;}
+            """
+            )
 
         if title := parent.windowTitle():
             self.title.setText(title)
+            self.title.setFont(QFont("Titillim Web"))
         title_bar_layout.addWidget(self.title)
+
         # Min button
         self.min_button = QToolButton(self)
         min_icon = QIcon()
@@ -47,12 +53,15 @@ class CustomTitleBar(QWidget):
         max_icon = QIcon()
         max_icon.addFile("resources/max.svg")
         self.max_button.setIcon(max_icon)
+        self.max_button.setIconSize(QSize(24, 24))
         self.max_button.clicked.connect(self.window().showMaximized)
 
         # Close button
         self.close_button = QToolButton(self)
         close_icon = QIcon()
-        close_icon.addFile("resources/close.svg")  # Close has only a single state.
+        close_icon.addFile("resources/close.svg")
+
+        # Close has only a single state.
         self.close_button.setIcon(close_icon)
         self.close_button.clicked.connect(self.window().close)
 
@@ -72,14 +81,16 @@ class CustomTitleBar(QWidget):
         ]
         for button in buttons:
             button.setFocusPolicy(Qt.FocusPolicy.NoFocus)
-            button.setFixedSize(QSize(16, 16))
+            button.setFixedSize(QSize(35, 35))
             button.setStyleSheet(
                 """QToolButton {
                     border: none;
                     padding: 2px;
+                    color: black;
                 }
                 """
             )
+            button.setIconSize(QSize(26, 26))
             title_bar_layout.addWidget(button)
 
     def window_state_changed(self, state):
@@ -94,27 +105,36 @@ class CustomTitleBar(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Custom Title Bar")
-        self.resize(400, 200)
+        self.setWindowTitle("A Semi-complete Guide to PyQt6")
+        self.resize(700, 180)
         self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
         self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
         central_widget = QWidget()
         # This container holds the window contents, so we can style it.
         central_widget.setObjectName("Container")
-        central_styles = "#Container {background: qlineargradient(x1:0"
-        central_styles += f"y1:0, x2:1 y2:1, stop:0 {python_blue}"
-        central_styles += f"stop:1 {python_light_blue}); border-radius: 5px;"
         central_widget.setStyleSheet(
             """#Container {
-            background: qlineargradient(x1:0 y1:0, x2:1 y2:1, stop:0 #306998 stop:1 #4B8BBE);
-            border-radius: 5px; border: 1px solid black;
+            background: qlineargradient(x1:0 y1:0, x2:0 y2:1,
+            stop:1 #4584B6 stop:0 #4584B6);
+            border-radius: 5px; border: 4px solid black;
+            padding: 12px;
         }"""
         )
+        self.set_fonts()
         self.title_bar = CustomTitleBar(self)
 
         work_space_layout = QVBoxLayout()
-        work_space_layout.setContentsMargins(11, 11, 11, 11)
-        work_space_layout.addWidget(QLabel("Hello, World!", self))
+        work_space_layout.setContentsMargins(30, 5, 11, 11)
+
+        # Set Series Title
+        main_title = QLabel("Intro: Getting Started!")
+        main_title.setFont(QFont("Titillium Web", 46, 600))
+        main_title_styles = "color: #fff;"
+        main_title.setStyleSheet(main_title_styles)
+        # add_drop_shadow(main_title, 1, "#111", 1, 1)
+
+        work_space_layout.addWidget(main_title)
+        # work_space_layout.addWidget(video_title)
 
         centra_widget_layout = QVBoxLayout()
         centra_widget_layout.setContentsMargins(0, 0, 0, 0)
@@ -155,6 +175,32 @@ class MainWindow(QMainWindow):
         self.initial_pos = None
         super().mouseReleaseEvent(event)
         event.accept()
+
+    def set_fonts(self):
+        # import fonts
+        font_dir = "resources/fonts/"
+        heading_font_name = "TitilliumWeb-Black.ttf"
+        heading_font_path = font_dir + heading_font_name
+
+        regular_font_name = "TitilliumWeb-Regular.ttf"
+        regular_font_path = font_dir + regular_font_name
+
+        # Try and add fonts
+        success = QFontDatabase.addApplicationFont(heading_font_path)
+        if success == -1:
+            print(f"{heading_font_name} not loaded")
+        success = QFontDatabase.addApplicationFont(regular_font_path)
+        if success == -1:
+            print("Regular font not loaded.")
+
+
+def add_drop_shadow(label: QLabel, radius: int,
+                    color: str, x_off: int, y_off: int) -> None:
+    shadow = QGraphicsDropShadowEffect()
+    shadow.setBlurRadius(radius)
+    shadow.setColor(QColor(color))
+    shadow.setOffset(x_off, y_off)
+    label.setGraphicsEffect(shadow)
 
 
 if __name__ == "__main__":
